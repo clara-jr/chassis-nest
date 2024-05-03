30ab04f7a1888ea61447ae21bfe2070800db9f6e
clara-jr <clarajimenezrecio@gmail.com>
2024-05-03 21:18:16 +0200

    fix: some eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
 import globals from 'globals';
 import js from '@eslint/js';
 import jsonformat from 'eslint-plugin-json-format';
-import prettier from 'eslint-plugin-prettier';
 import tseslint from 'typescript-eslint';
 import tsparser from '@typescript-eslint/parser';
 
 export default [
   js.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettier.configs.recommended, // Recommended config applied to all files
+  ...tseslint.configs.recommended, // Recommended config applied to all files
   // Override the recommended config
   {
     languageOptions: {
@@ -31,20 +29,13 @@ export default [
       'json-format': jsonformat,
     },
     rules: {
-      indent: ['error', 2],
+      indent: ['error', 2, { 'SwitchCase': 1, 'ignoredNodes': ['PropertyDefinition'] }],
       'linebreak-style': ['error', 'unix'],
       quotes: ['error', 'single'],
       semi: ['error', 'always'],
       'object-curly-spacing': ['error', 'always'],
       'space-in-parens': ['error', 'never'],
-      'no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
+      'no-unused-vars': 'off',
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
